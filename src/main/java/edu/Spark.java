455891cabda6dbf1d41a86915c9227723cdb1607
kleer <entrenamos@kleer.la>
2015-02-13 19:44:48 -0500

    modifique spark e index

--- src/main/java/edu/Spark.java
+++ src/main/java/edu/Spark.java
@@ -7,22 +7,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Spark {
 
     public static void main(String[] args) {
+    	
+    	get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("portal", "PORTAL DE IGLESIAS");
+            model.put("welcome", "BIENVENIDOS");
+            return new ModelAndView(model, "index.wm");
+        }, new VelocityTemplateEngine());
 
-//        get("/", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            model.put("welcome", "Hola Mundo");
-//            return new ModelAndView(model, "index.wm");
-//        }, new VelocityTemplateEngine());
-
-
-        get("/",(request, response) -> {
-            return "<h1 id=\"welcome\">Hola Mundo</h1>";
-        });
-
+       /* post("/login", (request, response) -> {
+        	
+        	String usuario = request.queryParams("usuario");
+        	String password = request.queryParams("password");
+        	
+        	loginValidator loginValidator = new loginValidator();  	
+        	
+            Map<String, Object> model = new HashMap<>();
+            if(loginValidator.valide(usuario, password)){
+            model.put("saludo", "Bienvenido a Ananzon, " + usuario);
+            }
+            return new ModelAndView(model, "login.wm");
+        }, new VelocityTemplateEngine());
+        */
     }
+
 
 }
